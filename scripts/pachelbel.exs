754adc5fec56716bc183e4edfce6f244a6e0a6b0
Bill Allen <bill@k2bea.org>
2026-09-21 18:10:02 -0400

    old files

--- scripts/pachelbel.exs
+++ scripts/pachelbel.exs
@@ -2,6 +2,10 @@ Mix.install([
   {:music_build, github: "bwanab/music_build"},
   {:sapf_server, path: "."}
 ])
+synth_file = case System.argv() do
+  [] -> "synths/basic_synth.sapf"
+  [file] -> file
+end
 
 chords = MusicBuild.Examples.ArpeggioProgressions.build_chords([:I, :V, :vi, :iii, :IV, :I, :IV, :V], :C, 3, 1, 0)
 patterns = [
@@ -22,4 +26,9 @@ stm = %{0 => STrack.new(arpeggios, name: "arpeggios", tpqn: 960, type: :instrume
 SAPFServer.start_link(0)
 SAPFServer.midi_start()
 SAPFServer.connect_input()
-SAPFServer.play(stm, synth_file: "synths/basic_synth.sapf")
+SAPFServer.send_control_message(20, 10)
+SAPFServer.send_control_message(21, 10)
+SAPFServer.send_control_message(22, 10)
+SAPFServer.send_control_message(23, 10)
+SAPFServer.play(stm, synth_file: synth_file)
+SAPFServer.quit()
